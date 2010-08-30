41e05b5e1dd4d5e4bd74f0327ff1c09cef65fadb
Paulo Machado <machado@zlw-ima-rwth-aachen.de>
2010-08-30 17:38:34 +0200

    Added test based on ping and nestat statistic

--- connection_manager.sh
+++ connection_manager.sh
@@ -6,6 +6,8 @@
 CONF=tmobile
 LOGF=/var/log/connection.log
 LOGCMD="logger -s"
+T0=0
+T1=0
 
 connect() {
 	$LOGCMD "Starting wvdial connection"
@@ -31,6 +33,13 @@ terminate() {
 	[ -n $? ] && [ $? -ne 0 ] && terminate
 }
 
+update_diff_counter() {
+	T0=$(netstat -st|grep -A 8 Tcp:|grep "segments received"|cut -d" " -f 5)
+	ping -c1 google.com &> /dev/null
+	sleep 1
+	let T1=$(netstat -st|grep -A 8 Tcp:|grep "segments received"|cut -d" " -f 5)-$T0
+}
+
 check() {
 	# 1st check: Are the commands running?
 	WPID=$(pgrep wvdial)
@@ -39,9 +48,10 @@ check() {
 		$LOGCMD "pppd and/or dialer not running"
 		RET=1
 	# 2nd check: the connection is really up?
-	# TODO: best method? pings? AT commands?
-	#elif [ ] ; then
-		
+	# TODO: pings tunnel? AT commands?
+	update_diff_counter	
+	elif [ $T1 -eq 0 ]; then
+		RET=1
 	else 
 		RET=0
 	fi
@@ -55,6 +65,7 @@ connect
 # Main loop
 while true; do
 # different treatment to different problems
+	sleep 7
 	check 
 	[ $RET -eq 1 ] && reconnect
 	[ $RET -eq 2 ] && /sbin/reboot
